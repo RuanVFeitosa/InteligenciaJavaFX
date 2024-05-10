039c94bdf9557f4351577bea0adc3ac9df38e0ce
RuanVFeitosa <vianafeitosaruan@gmail.com>
2024-05-10 16:38:02 -0300

    Login e Dashboard

---
+++ src/controller/LoginController.java
@@ -0,0 +1,62 @@
+package controller;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class LoginController {
+	
+	@FXML
+	private TextField usuario;
+	
+	@FXML
+	private PasswordField senha;
+	
+	
+	public void entrar() {
+		System.out.println("Tentativa de login... ");
+		
+		//Logica de entrada
+		if (usuario.getText().equals("admin") && senha.getText().equals("admin")) {
+		
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/dashboard.fxml"));
+			
+			try {
+				
+				StackPane root = loader.load();
+				
+				Scene scene = new Scene(root, 600, 400);
+				
+				Stage currentStage = (Stage) usuario.getScene().getWindow();
+				
+				currentStage.setScene(scene);
+				currentStage.setTitle("Dashboard");
+				currentStage.show();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		} else {
+			mensagemErro();
+		}
+	}
+	
+	public void mensagemErro() {
+		Alert alert = new Alert(Alert.AlertType.ERROR);
+		alert.setTitle("Sua senha está errada");
+		alert.setContentText("Senha Incorreta");
+		alert.setHeaderText(null);
+		alert.showAndWait();
+		
+	}
+	
+}
---
+++ src/model/Agentes.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Agentes {
+	
+	private int id;
+	
+	private String nome;
+	private String inicioAgente;
+	private String fimAgente;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getInicioAgente() {
+		return inicioAgente;
+	}
+	public void setInicioAgente(String inicioAgente) {
+		this.inicioAgente = inicioAgente;
+	}
+	public String getFimAgente() {
+		return fimAgente;
+	}
+	public void setFimAgente(String fimAgente) {
+		this.fimAgente = fimAgente;
+	}
+	
+	
+
+}
